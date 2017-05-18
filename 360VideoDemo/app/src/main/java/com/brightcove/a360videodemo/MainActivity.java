ad504a009b4edd747fe258ab6a3c7b35b7804331
Raaj Subramaniam <rsubramaniam@brightcove.com>
2017-05-18 14:37:41 -0400

    Updated the video and launcher icon

--- 360VideoDemo/app/src/main/java/com/brightcove/a360videodemo/MainActivity.java
+++ 360VideoDemo/app/src/main/java/com/brightcove/a360videodemo/MainActivity.java
@@ -1,8 +1,9 @@
 package com.brightcove.a360videodemo;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Handler;
+import android.view.View;
 
 import com.brightcove.player.event.Event;
 import com.brightcove.player.event.EventListener;
@@ -10,41 +11,72 @@ import com.brightcove.player.event.EventType;
 import com.brightcove.player.media.DeliveryType;
 import com.brightcove.player.model.Video;
 import com.brightcove.player.view.BrightcoveExoPlayerVideoView;
+import com.brightcove.player.view.BrightcovePlayer;
 
-public class MainActivity extends AppCompatActivity {
-    private BrightcoveExoPlayerVideoView videoPlayer;
+
+public class MainActivity extends BrightcovePlayer {
+
+    private final Handler handler = new Handler();
+    private View decorView;
 
     @Override
     @SuppressWarnings("ResourceType")
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        // When extending the BrightcovePlayer, we must assign the brightcoveVideoView before
+        // entering the superclass. This allows for some stock video player lifecycle
+        // management.  Establish the video object and use it's event emitter to get important
+        // notifications and to control logging.
         setContentView(R.layout.activity_main);
+        brightcoveVideoView = (BrightcoveExoPlayerVideoView) findViewById(R.id.brightcove_video_view);
+        super.onCreate(savedInstanceState);
 
-        videoPlayer = (BrightcoveExoPlayerVideoView) findViewById(R.id.video_player);
-        videoPlayer.getEventEmitter().on(EventType.CHANGE_ORIENTATION, new EventListener() {
+
+        Video video = Video.createVideo(
+                "asset:///sample360.mp4", DeliveryType.MP4, Video.ProjectionFormat.EQUIRECTANGULAR);
+        brightcoveVideoView.add(video);
+
+        decorView = getWindow().getDecorView();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        brightcoveVideoView.getEventEmitter().emit(EventType.ACTIVITY_PAUSED);
+    }
+
+    @Override
+    @SuppressWarnings("ResourceType")
+    protected void onResume() {
+        super.onResume();
+        brightcoveVideoView.getEventEmitter().on(EventType.CHANGE_ORIENTATION, new EventListener() {
             @Override
             public void processEvent(Event event) {
                 int orientation = event.getIntegerProperty(Event.REQUESTED_ORIENTATION);
                 setRequestedOrientation(orientation);
             }
         });
-
-        Video video = Video.createVideo(
-                "asset:///sample360.mp4", DeliveryType.MP4, Video.ProjectionFormat.EQUIRECTANGULAR);
-        videoPlayer.add(video);
+        brightcoveVideoView.getEventEmitter().emit(EventType.ACTIVITY_RESUMED);
     }
+
+    private final Runnable hideSystemUiTask = new Runnable() {
+        @Override
+        public void run() {
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    };
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        videoPlayer.getEventEmitter().emit(EventType.ACTIVITY_PAUSED);
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            handler.postDelayed(hideSystemUiTask, 500);
+        }
     }
 
-
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        videoPlayer.getEventEmitter().emit(EventType.ACTIVITY_RESUMED);
-    }
 }
