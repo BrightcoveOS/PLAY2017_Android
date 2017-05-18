0d145a751b1b1747240818db817ca0685e460a64
Raaj Subramaniam <rsubramaniam@brightcove.com>
2017-05-18 14:55:40 -0400

    Fixed AND-693 - Moved logic for setting the orientation change listener from onResume to onCreate. - Remove onPause and onResume overrides, because they are handled automatically by the BrightcovePlayer class

--- 360VideoDemo/app/src/main/java/com/brightcove/a360videodemo/MainActivity.java
+++ 360VideoDemo/app/src/main/java/com/brightcove/a360videodemo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.brightcove.a360videodemo;
 
 
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -22,14 +23,20 @@ public class MainActivity extends BrightcovePlayer {
     @Override
     @SuppressWarnings("ResourceType")
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         // When extending the BrightcovePlayer, we must assign the brightcoveVideoView before
         // entering the superclass. This allows for some stock video player lifecycle
         // management.  Establish the video object and use it's event emitter to get important
         // notifications and to control logging.
         setContentView(R.layout.activity_main);
         brightcoveVideoView = (BrightcoveExoPlayerVideoView) findViewById(R.id.brightcove_video_view);
-        super.onCreate(savedInstanceState);
-
+        brightcoveVideoView.getEventEmitter().on(EventType.CHANGE_ORIENTATION, new EventListener() {
+            @Override
+            public void processEvent(Event event) {
+                int orientation = event.getIntegerProperty(Event.REQUESTED_ORIENTATION);
+                setRequestedOrientation(orientation);
+            }
+        });
 
         Video video = Video.createVideo(
                 "asset:///sample360.mp4", DeliveryType.MP4, Video.ProjectionFormat.EQUIRECTANGULAR);
@@ -38,36 +45,18 @@ public class MainActivity extends BrightcovePlayer {
         decorView = getWindow().getDecorView();
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        brightcoveVideoView.getEventEmitter().emit(EventType.ACTIVITY_PAUSED);
-    }
-
-    @Override
-    @SuppressWarnings("ResourceType")
-    protected void onResume() {
-        super.onResume();
-        brightcoveVideoView.getEventEmitter().on(EventType.CHANGE_ORIENTATION, new EventListener() {
-            @Override
-            public void processEvent(Event event) {
-                int orientation = event.getIntegerProperty(Event.REQUESTED_ORIENTATION);
-                setRequestedOrientation(orientation);
-            }
-        });
-        brightcoveVideoView.getEventEmitter().emit(EventType.ACTIVITY_RESUMED);
-    }
-
     private final Runnable hideSystemUiTask = new Runnable() {
         @Override
         public void run() {
-            decorView.setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                decorView.setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+            }
         }
     };
 
